65284c44abb889ed9244107e671bfc9d745871e4
Adrian Chang <adrian_chang@brown.edu>
2023-05-04 16:50:07 -0400

    dataset flickr code

---
+++ code/dataset.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import os
+from PIL import Image
+import numpy as np
+
+class FlickrDataset(Dataset):
+    def __init__(self, base_filepath : str):
+        annotation_filepath = os.path.join(base_filepath, "Flickr8k_text")
+        expert = pd.read_csv(
+            os.path.join(annotation_filepath, "ExpertAnnotations.txt"),
+            sep='\t',
+            header=None
+        )
+        crowd = pd.read_csv(
+            os.path.join(annotation_filepath, "CrowdFlowerAnnotations.txt"),
+            sep='\t',
+            header=None
+        )
+        annotations = pd.read_csv(
+            os.path.join(annotation_filepath, "Flickr8k.token.txt"),
+            sep='\t',
+            header=None
+        )
+
+        expert['score'] = expert[[2, 3, 4]].mean(axis=1) / 4
+        expert.drop(columns=[2, 3, 4])
+        crowd['score'] = crowd[2]
+        crowd.drop(columns=[2, 3, 4])
+
+        human_annotations = pd.concat([crowd, expert])
+        human_annotations.sort_values('score').drop_duplicates(subset=[0, 1], keep='last')
+
+        self.img_folder = os.path.join(base_filepath, "Flicker8k_Dataset")
+        self.img_filepaths = list(human_annotations[0])
+        self.caption_ids = list(human_annotations[1])
+        self.human_scores = list(human_annotations[2])
+        self.annotations = dict(zip(annotations[0], annotations[1]))
+
+    def __len__(self):
+        return len(self.img_filepaths)
+
+    def __getitem__(self, idx : int):
+        img_filepath = self.img_filepaths[idx]
+        caption_id = self.caption_ids[idx]
+        human_score = self.human_scores[idx]
+        caption = self.annotations[caption_id]
+        img = np.asarray(Image.open(os.path.join(self.img_folder, img_filepath)))
+
+        return img, caption, human_score
